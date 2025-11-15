58f6acd1070e465b77db7a24bf9a04406b7577cf
Ralf S. Engelschall <rse@engelschall.com>
2025-11-15 12:13:25 +0100

    first cut for parsing, card and prose formatting and output generation

---
+++ rulebook-3-cli/src/rulebook-repository.ts
@@ -0,0 +1,115 @@
+/*
+**  Rulebook - Policy Rule Rendering Application
+**  Copyright (c) 2025 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
+*/
+
+import { parseDocument } from "yaml"
+
+/*  internal dependencies  */
+import { type IndexType, type AspectType } from "./rulebook-schema"
+import { RulebookParser }                  from "./rulebook-parser"
+import { RulebookGenerator }               from "./rulebook-generator"
+import { RulebookSerializer }              from "./rulebook-serializer"
+
+/*  CST type  */
+export type RulebookArtifactCST = ReturnType<typeof parseDocument>
+
+/*  artifact container  */
+export class RulebookArtifact<T extends object> {
+    constructor (
+        public obj:  T,
+        public file  = "",
+        public yaml  = "",
+        public cst:  RulebookArtifactCST | null = null
+    ) {}
+}
+
+/*  rulebook repository  */
+export class RulebookRepository {
+    /*  internal state  */
+    private index:   RulebookArtifact<IndexType> | null = null
+    private aspects: RulebookArtifact<AspectType>[] = []
+
+    /*  parser, generator, and serializer  */
+    private parser:     RulebookParser
+    private generator:  RulebookGenerator
+    private serializer: RulebookSerializer
+
+    /*  constructor  */
+    constructor () {
+        this.parser     = new RulebookParser(this)
+        this.generator  = new RulebookGenerator(this)
+        this.serializer = new RulebookSerializer(this)
+    }
+
+    /*  create artifact (used by parser)  */
+    createArtifact<T extends object> (
+        obj:  T,
+        file: string,
+        yaml: string,
+        cst:  RulebookArtifactCST
+    ): RulebookArtifact<T> {
+        return new RulebookArtifact(obj, file, yaml, cst)
+    }
+
+    /*  access to index (used by parser and generator)  */
+    getIndex (): RulebookArtifact<IndexType> | null {
+        return this.index
+    }
+
+    /*  set index (used by parser)  */
+    setIndex (index: RulebookArtifact<IndexType>) {
+        this.index = index
+    }
+
+    /*  access to aspects (used by parser and generator)  */
+    getAspects (): RulebookArtifact<AspectType>[] {
+        return this.aspects
+    }
+
+    /*  add aspect (used by parser)  */
+    addAspect (aspect: RulebookArtifact<AspectType>) {
+        this.aspects.push(aspect)
+    }
+
+    /*  find aspect by id (used by parser)  */
+    findAspectById (id: string): RulebookArtifact<AspectType> | undefined {
+        return this.aspects.find((aspect) => aspect.obj.Id === id)
+    }
+
+    /*  clear aspects (used by serializer)  */
+    clearAspects () {
+        this.aspects = []
+    }
+
+    /*  export rulebook to JSON  */
+    export () {
+        return this.serializer.export()
+    }
+
+    /*  import rulebook from JSON  */
+    import (json: string) {
+        this.serializer.import(json)
+    }
+
+    /*  parse index YAML specification  */
+    parseIndex (file: string, yaml: string) {
+        this.parser.parseIndex(file, yaml)
+    }
+
+    /*  parse aspect YAML specification  */
+    parseAspect (file: string, yaml: string) {
+        this.parser.parseAspect(file, yaml)
+    }
+
+    /*  validate cross-references  */
+    validateCrossRefs () {
+        this.parser.validateCrossRefs()
+    }
+
+    /*  generate HTML output  */
+    async print (format = "card") {
+        return this.generator.print(format)
+    }
+}
--- rulebook-3-cli/src/rulebook.ts
+++ rulebook-3-cli/src/rulebook.ts
@@ -4,8 +4,9 @@
 **  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
 */
 
-/*  internal dependencies  */
-// import path              from "node:path"
+/*  built-in dependencies  */
+import path              from "node:path"
+import fs                from "node:fs"
 
 /*  external dependencies  */
 import CLIio             from "cli-io"
@@ -13,49 +14,116 @@ import syspath           from "syspath"
 import yargs             from "yargs"
 import { hideBin }       from "yargs/helpers"
 import chalk             from "chalk"
-// import jsYAML            from "js-yaml"
-// import { type }          from "arktype"
+import { glob }          from "glob"
 
 /*  internal dependencies  */
 // @ts-ignore
-import pkgJSON           from "../../package.json?raw" with { type: "json" }
+import pkgJSON                from "../../package.json?raw" with { type: "json" }
+import { RulebookRepository } from "./rulebook-repository"
+import { RulebookParseError } from "./rulebook-parser"
 
 /*  central CLI context  */
 let cli: CLIio | null = null
 
+/*  parse rulebook  */
+const parseRulebook = async (cli: CLIio, dir: string) => {
+    /*  create rulebook repository  */
+    const rulebook = new RulebookRepository()
+
+    /*  sanity check source directory  */
+    cli.log("info", `loading rulebook source "${dir}"`)
+    const stat = await fs.promises.stat(dir).catch(() => null)
+    if (stat === null)
+        throw new Error(`rulebook source path ${dir} not existing`)
+    if (!stat.isDirectory())
+        throw new Error(`rulebook source path ${dir} not a directory`)
+
+    /*  read index  */
+    const indexFile = path.join(dir, "INDEX.yaml")
+    cli.log("info", `loading rulebook index  "${indexFile}"`)
+    const exists = await fs.promises.access(indexFile, fs.constants.R_OK)
+        .then(() => true).catch(() => false)
+    if (!exists)
+        throw new Error(`rulebook index "${indexFile}" not found`)
+    const indexYAML = await fs.promises.readFile(indexFile, "utf8")
+    try {
+        rulebook.parseIndex(indexFile, indexYAML)
+    }
+    catch (err: any) {
+        if (err instanceof RulebookParseError)
+            cli.log("error", err.render(true))
+        else
+            cli.log("error", String(err))
+        throw new Error("failed to parse rulebook index")
+    }
+
+    /*  iterate over all aspects  */
+    const aspectFiles = (await glob([ "*.yaml" ], { cwd: dir }))
+        .filter((p) => p !== "INDEX.yaml")
+        .map((p) => path.join(dir, p))
+        .toSorted()
+    for (const aspectFile of aspectFiles) {
+        cli.log("info", `loading rulebook aspect "${aspectFile}"`)
+        const aspectYAML = await fs.promises.readFile(aspectFile, "utf8")
+        try {
+             rulebook.parseAspect(aspectFile, aspectYAML)
+        }
+        catch (err: any) {
+            if (err instanceof RulebookParseError)
+                cli.log("error", err.render(true))
+            else
+                cli.log("error", String(err))
+            throw new Error("failed to parse rulebook aspect")
+        }
+    }
+
+    /*  validate rulebook  */
+    cli.log("info", "validating rulebook cross-references")
+    rulebook.validateCrossRefs()
+
+    return rulebook
+}
+
+/*  format rulebook  */
+const formatRulebook = async (cli: CLIio, rulebook: RulebookRepository, format: string) => {
+    cli.log("info", `formatting rulebook into format "${format}"`)
+    const output = await rulebook.print(format)
+    return output
+}
+
 /*  command handler: make  */
-async function cmdMake (
+const cmdMake = async (
+    cli:        CLIio,
     globalOpts: { verbose: boolean },
-    cmdOpts:    { output: string },
+    cmdOpts:    { output: string, format: string },
     cmdArgs:    { dir: string }
-) {
-    if (cli === null)
-        throw new Error("CLI not initialized")
-    cli.log("info", `make: output=${cmdOpts.output} dir=${cmdArgs.dir} verbose=${globalOpts.verbose}`)
-    /*  TODO: implement make functionality  */
+) => {
+    cli.log("info", `make: output=${cmdOpts.output} format=${cmdOpts.format} dir=${cmdArgs.dir} verbose=${globalOpts.verbose}`)
+    const rulebook = await parseRulebook(cli, cmdArgs.dir)
+    const output = await formatRulebook(cli, rulebook, cmdOpts.format)
+    cli.log("info", `saving rulebook output "${cmdOpts.output}"`)
+    await fs.promises.writeFile(cmdOpts.output, output, "utf8")
 }
 
 /*  command handler: serve  */
-async function cmdServe (
+const cmdServe = async (
+    cli:        CLIio,
     globalOpts: { verbose: boolean },
     cmdOpts:    { addr: string, port: number },
     cmdArgs:    { dir: string }
-) {
-    if (cli === null)
-        throw new Error("CLI not initialized")
+) => {
     cli.log("info", `serve: address=${cmdOpts.addr} port=${cmdOpts.port} dir=${cmdArgs.dir} verbose=${globalOpts.verbose}`)
     /*  TODO: implement serve functionality  */
 }
 
 /*  command handler: preview  */
-async function cmdPreview (
+const cmdPreview = async (
+    cli:        CLIio,
     globalOpts: { verbose: boolean },
-    cmdOpts:    { addr: string, port: number },
+    cmdOpts:    { format: string, addr: string, port: number },
     cmdArgs:    { dir: string }
-) {
-    if (cli === null)
-        throw new Error("CLI not initialized")
-    cli.log("info", `preview: address=${cmdOpts.addr} port=${cmdOpts.port} dir=${cmdArgs.dir} verbose=${globalOpts.verbose}`)
+) => {
+    cli.log("info", `preview: format=${cmdOpts.format} address=${cmdOpts.addr} port=${cmdOpts.port} dir=${cmdArgs.dir} verbose=${globalOpts.verbose}`)
     /*  TODO: implement preview functionality  */
 }
 
@@ -68,20 +136,23 @@ async function cmdPreview (
         dataDirAutoCreate: true
     })
 
+    /*  establish CLI environment  */
+    const setupCLI = (verbose: boolean) => {
+        const logLevel = verbose ? "info" : "warning"
+        cli = new CLIio({
+            encoding:  "utf8",
+            logLevel,
+            logTime:   false,
+            logPrefix: "rulebook"
+        })
+    }
+
     /*  parse command-line arguments  */
     const coerce = (arg: string) => Array.isArray(arg) ? arg[arg.length - 1] : arg
     const args = await yargs()
         /* eslint @stylistic/indent: off */
         .usage("Usage: $0 [<global-options>] <command> [<command-options>] [<command-arguments>]")
         .version(false)
-        .option("version", {
-            alias:    "V",
-            type:     "boolean",
-            array:    false,
-            coerce,
-            default:  false,
-            describe: "show program version information"
-        })
         .option("verbose", {
             alias:    "v",
             type:     "boolean",
@@ -89,6 +160,13 @@ async function cmdPreview (
             coerce,
             default:  false,
             describe: "enable verbose logging"
+        })
+        .command("version", "show program version", (yargs) => yargs, async (argv: any) => {
+            const pkg = JSON.parse(pkgJSON)
+            process.stderr.write(`Rulebook ${pkg.version} <${pkg.homepage}>\n`)
+            process.stderr.write(`Copyright (c) 2025 ${pkg.author.name} <${pkg.author.url}>\n`)
+            process.stderr.write(`Licensed under ${pkg.license} <http://spdx.org/licenses/${pkg.license}.html>\n`)
+            process.exit(0)
         })
         .command("make <dir>", "make rulebook rendering from source", (yargs) => {
             return yargs
@@ -100,15 +178,32 @@ async function cmdPreview (
                     demandOption: true,
                     describe:     "output directory"
                 })
+                .option("format", {
+                    alias:        "f",
+                    type:         "string",
+                    array:        false,
+                    nargs:        1,
+                    default:      "card",
+                    describe:     "output format (\"card\", \"prose\", \"app\")"
+                })
                 .positional("dir", {
                     type:         "string",
                     describe:     "rulebook source directory"
                 })
-        }, async (argv: any) => cmdMake(
-            { verbose: argv.verbose },
-            { output:  argv.output },
-            { dir:     argv.dir }
-        ))
+        }, async (argv: any) => {
+            setupCLI(argv.verbose)
+            cmdMake(cli!,
+                { verbose: argv.verbose },
+                { output:  argv.output, format: argv.format },
+                { dir:     argv.dir }
+            ).catch((err) => {
+                if (err instanceof Error)
+                    cli!.log("error", err.message)
+                else
+                    cli!.log("error", String(err))
+                process.exit(0)
+            })
+        })
         .command("serve <dir>", "serve rulebook rendering", (yargs) => {
             return yargs
                 .option("address", {
@@ -131,13 +226,24 @@ async function cmdPreview (
                     type:     "string",
                     describe: "rulebook source directory"
                 })
-        }, async (argv: any) => cmdServe(
-            { verbose: argv.v },
-            { addr: argv.a, port: argv.p },
-            { dir: argv.dir }
-        ))
+        }, async (argv: any) => {
+            setupCLI(argv.verbose)
+            return cmdServe(cli!,
+                { verbose: argv.verbose },
+                { addr:    argv.addr, port: argv.port },
+                { dir:     argv.dir }
+            )
+        })
         .command("preview <dir>", "preview rulebook rendering from source", (yargs) => {
             return yargs
+                .option("format", {
+                    alias:        "f",
+                    type:         "string",
+                    array:        false,
+                    nargs:        1,
+                    default:      "card",
+                    describe:     "output format (\"card\", \"prose\", \"app\")"
+                })
                 .option("address", {
                     alias:    "a",
                     type:     "string",
@@ -158,35 +264,20 @@ async function cmdPreview (
                     type:     "string",
                     describe: "rulebook source directory"
                 })
-        }, async (argv: any) => cmdPreview(
-            { verbose: argv.v },
-            { addr: argv.a, port: argv.p },
-            { dir: argv.dir }
-        ))
+        }, async (argv: any) => {
+            setupCLI(argv.verbose)
+            return cmdPreview(cli!,
+                { verbose: argv.verbose },
+                { format:  argv.format, addr: argv.addr, port: argv.port },
+                { dir:     argv.dir }
+            )
+        })
         .help("h", "show usage help")
         .alias("h", "help")
         .showHelpOnFail(true)
         .strict()
         .demandCommand(1, "Please specify a command")
         .parse(hideBin(process.argv))
-
-    /*  short-circuit version request  */
-    if (args.V) {
-        const pkg = JSON.parse(pkgJSON)
-        process.stderr.write(`Rulebook ${pkg.version} <${pkg.homepage}>\n`)
-        process.stderr.write(`Copyright (c) 2025 ${pkg.author.name} <${pkg.author.url}>\n`)
-        process.stderr.write(`Licensed under ${pkg.license} <http://spdx.org/licenses/${pkg.license}.html>\n`)
-        process.exit(0)
-    }
-
-    /*  establish CLI environment  */
-    const logLevel = args.v ? "info" : "warning"
-    cli = new CLIio({
-        encoding:  "utf8",
-        logLevel,
-        logTime:   false,
-        logPrefix: "rulebook"
-    })
 })().catch((err: Error) => {
     if (cli !== null)
         cli.log("error", err.message)
